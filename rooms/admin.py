0da71ce003e864b0cb40e8c0fa805cd4388e967a
hosibito <hosibito@naver.com>
2022-10-02 14:50:18 +0900

    8.1 Search Fields(admin)

--- rooms/admin.py
+++ rooms/admin.py
@@ -26,6 +26,12 @@ class RoomAdmin(admin.ModelAdmin):
         "updated_at",
     )
 
+    search_fields = (
+        "name",
+        "^price",
+        "=owner__username",
+    )
+
     # # 이게 어드민에 있을지 모델이 있을지를 잘 결정해야 한다.
     # def total_amenities(self, room):
     #     return room.amenities.count()
